b40a4f59a67488ce14c1323e05a53482bc0f489b
Shane-M-Thompson <111658609+Shane-M-Thompson@users.noreply.github.com>
2022-08-25 08:56:34 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Shane Thompson
+ * Date: 08/25/2022
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Shane Thompson!\n");
+  printf("Computer Science!\n");
+  
+  return 0;
+}
